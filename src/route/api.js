765e35fd59f4576e1add39630231d3fe7c8664c9
Taufik Riskyanto <taufik.riskyanto@doku.com>
2024-06-19 08:04:59 +0700

    Create Contact JS

--- src/route/api.js
+++ src/route/api.js
@@ -1,12 +1,18 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
 import { authMiddleware } from "../middleware/auth-middleware.js";
+import contactController from "../controller/contact-controller.js";
 const userRouter = new express.Router();
 userRouter.use(authMiddleware);
+
+//USER
 userRouter.get('/api/users/current', userController.get);
 userRouter.patch('/api/users/current', userController.update)
 userRouter.delete('/api/users/logout', userController.logout);
 
+
+//CONTACT
+userRouter.post('/api/contacts', contactController.create)
 export {
     userRouter
 }
